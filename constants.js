0388da6aaf8c34b57f2f095a790b500a8f1a02c4
HashemKhalifa <hashem.khalifa@gmail.com>
2021-07-01 10:59:22 +0200

    add more products

--- constants.js
+++ constants.js
@@ -1,139 +1,154 @@
 export const products = [
-    {
-        id: 1,
-        title: 'Neroli Organic',
-        href: './products/neroil',
-        category: { name: 'Essential oil', href: './products/neroil' },
-        imageUrl:
-            './products/Neroli.jpg',
-        preview:
-            'Neroli oil comes from the bitter orange tree, Citrus aurantium amara. The oil is steam-distilled from the delicate orange blossoms, which need to be handled with great care to avoid crushing the petals.',
-    },
-    {
-        id: 2,
-        title: 'Jasmine Absolute',
-        href: './products/jasminoil',
-        category: { name: 'Essential oil', href: './products/jasminoil' },
-        imageUrl:
-            './products/elmajd-jasmin.jpeg',
-        preview:
-            'Jasmine Absolute is an incredible oil known for its distinctive, exotic floral aroma that is often used in perfumery. Its intoxicating smell is known as an aphrodisiac that increases feelings of love and romance.',
-    },
-    {
-        id: 3,
-        title: 'Basil linalool',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/basil.jpg',
-        preview:
-            'Basil Oil Linalool (or Sweet Basil) is distilled from the botanical Ocimum basilicum L. The plant is typically grown and harvested for its leaves which are popular around the world in many food dishes. Seeds are planted late March and early April with cutting starting as early as June.',
-    },
-    {
-        id: 4,
-        title: 'Titgrain mandarin',
-        href: './products/titgrain-mandarin',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/titgrain-mandarin.jpg',
-        preview:
-            'titgrain mandarin provides a truly unique aromatic experience. This essential oil is obtained by steam distillation of the leaves and twigs of Citrus reticulata. Also the fruit produces mandarin essential oil in its varying forms.',
-    },
-    {
-        id: 5,
-        title: 'Coriander Herb',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/coriander-oil.jpg',
-        preview:
-            'Egypt are one of the largest producers of herb and based on history, perhaps they are also the oldest producer with historians tracing its origins back to ancient Egypt times. ',
-    },
+  {
+    id: 1,
+    title: 'Neroli Organic',
+    href: './products/neroil',
+    category: { name: 'Essential oil', href: './products/neroil' },
+    imageUrl: './products/Neroli.jpg',
+    preview:
+      'Neroli oil comes from the bitter orange tree, Citrus aurantium amara. The oil is steam-distilled from the delicate orange blossoms, which need to be handled with great care to avoid crushing the petals.',
+  },
+  {
+    id: 2,
+    title: 'Jasmine Absolute',
+    href: './products/jasminoil',
+    category: { name: 'Essential oil', href: './products/jasminoil' },
+    imageUrl: './products/elmajd-jasmin.jpeg',
+    preview:
+      'Jasmine Absolute is an incredible oil known for its distinctive, exotic floral aroma that is often used in perfumery. Its intoxicating smell is known as an aphrodisiac that increases feelings of love and romance.',
+  },
+  {
+    id: 3,
+    title: 'Basil linalool',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/basil.jpg',
+    preview:
+      'Basil Oil Linalool (or Sweet Basil) is distilled from the botanical Ocimum basilicum L. The plant is typically grown and harvested for its leaves which are popular around the world in many food dishes. Seeds are planted late March and early April with cutting starting as early as June.',
+  },
+  {
+    id: 4,
+    title: 'Titgrain mandarin',
+    href: './products/titgrain-mandarin',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/titgrain-mandarin.jpg',
+    preview:
+      'titgrain mandarin provides a truly unique aromatic experience. This essential oil is obtained by steam distillation of the leaves and twigs of Citrus reticulata. Also the fruit produces mandarin essential oil in its varying forms.',
+  },
+  {
+    id: 5,
+    title: 'Coriander Herb',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/coriander-oil.jpg',
+    preview:
+      'Egypt are one of the largest producers of herb and based on history, perhaps they are also the oldest producer with historians tracing its origins back to ancient Egypt times. ',
+  },
 
-    {
-        id: 6,
-        title: 'Geranium',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/geranium.jpg',
-        preview:
-            'geranium essential oil is derived by steam distillation of the leaves of Pelargonium graveolens, a plant species native to South Africa. According to folklore, it was used for a wide range of health conditions. ',
-    },
+  {
+    id: 6,
+    title: 'Geranium',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/geranium.jpg',
+    preview:
+      'geranium essential oil is derived by steam distillation of the leaves of Pelargonium graveolens, a plant species native to South Africa. According to folklore, it was used for a wide range of health conditions. ',
+  },
 
-    {
-        id: 7,
-        title: 'Cumin',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/cumin-oil.jpg',
-        preview:
-            'Egypt are a relatively small global producer of cumin seeds but a bigger contributor to the oil market. The soil is prepared late summer with the seeds planted in October. Harvesting and distillation occurs in May & June.',
-    },
+  {
+    id: 7,
+    title: 'Cumin',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/cumin-oil.jpg',
+    preview:
+      'Egypt are a relatively small global producer of cumin seeds but a bigger contributor to the oil market. The soil is prepared late summer with the seeds planted in October. Harvesting and distillation occurs in May & June.',
+  },
 
-    {
-        id: 8,
-        title: 'Marjoram',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/marjoram-oil.jpg',
-        preview:
-            'marjoram essential oil is a unique cooking additive with many additional internal and external benefits. The herbaceous flavoring of Marjoram oil can be used to spice up stews, dressings, soups, and meat dishes and can take the place of dried marjoram when cooking.',
-    },
+  {
+    id: 8,
+    title: 'Marjoram',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/marjoram-oil.jpg',
+    preview:
+      'marjoram essential oil is a unique cooking additive with many additional internal and external benefits. The herbaceous flavoring of Marjoram oil can be used to spice up stews, dressings, soups, and meat dishes and can take the place of dried marjoram when cooking.',
+  },
 
-    {
-        id: 9,
-        title: 'Fennel',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/funnel-oil.jpg',
-        preview:
-            'fennel essential oil, distilled from the fennel plant, has been shown to heal wounds, reduce and prevent spasms in the gut, contain antioxidant and antimicrobial characteristics, relieve gas and constipation, treat digestive issues, and aid in weight loss',
-    },
+  {
+    id: 9,
+    title: 'Fennel',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/funnel-oil.jpg',
+    preview:
+      'fennel essential oil, distilled from the fennel plant, has been shown to heal wounds, reduce and prevent spasms in the gut, contain antioxidant and antimicrobial characteristics, relieve gas and constipation, treat digestive issues, and aid in weight loss',
+  },
 
-    {
-        id: 10,
-        title: 'Organic blue chamomile',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/bluechamomile-oil.jpg',
-        preview:
-            ' Organic blue chamomile essential oil is steam distilled from Matricaria recutita flowers. This middle note oil has a heavily, sweet aroma with fresh and herbaceous undertones. ... The deep blue hue results from the natural azulene released during distillation.',
-    },
+  {
+    id: 10,
+    title: 'Organic blue chamomile',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/bluechamomile-oil.jpg',
+    preview:
+      ' Organic blue chamomile essential oil is steam distilled from Matricaria recutita flowers. This middle note oil has a heavily, sweet aroma with fresh and herbaceous undertones. ... The deep blue hue results from the natural azulene released during distillation.',
+  },
 
-    {
-        id: 11,
-        title: 'Lemongrass oil',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/lemongrass-oil.jpg',
-        preview:
-            "lemongrass oil Extracted from the leaves and stalks of the lemongrass plant, lemongrass oil has a powerful, citrus scent. It's often found in soaps and other personal care products. Lemongrass oil can be extracted, and it's been used by healthcare providers to treat digestive problems and high blood pressure.",
-    },
-    {
-        id: 12,
-        title: 'Lemon Petitgrain oil',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: './products/basiloil' },
-        imageUrl:
-            './products/Lemon-Petitgrain.jpg',
-        preview:
-            "Our organic Petitgrain Bigarade has a fresh, leafy-green, lightly floral, citrus aroma with light bitter/dry notes and rich, sweet undertones. This essential oil's Neroli-like aroma has made it a classic ingredient of Eau de Cologne formulas.",
-    },
+  {
+    id: 11,
+    title: 'Lemongrass oil',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/lemongrass-oil.jpg',
+    preview:
+      "lemongrass oil Extracted from the leaves and stalks of the lemongrass plant, lemongrass oil has a powerful, citrus scent. It's often found in soaps and other personal care products. Lemongrass oil can be extracted, and it's been used by healthcare providers to treat digestive problems and high blood pressure.",
+  },
+  {
+    id: 12,
+    title: 'Lemon Petitgrain oil',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/Lemon-Petitgrain.jpg',
+    preview:
+      "Our organic Petitgrain Bigarade has a fresh, leafy-green, lightly floral, citrus aroma with light bitter/dry notes and rich, sweet undertones. This essential oil's Neroli-like aroma has made it a classic ingredient of Eau de Cologne formulas.",
+  },
 
-    {
-        id: 12,
-        title: 'SOON  Neroli grapfruit',
-        href: './products/basiloil',
-        category: { name: 'Essential oil', href: '#' },
-        imageUrl:
-            './products/neroli-grapfruit.jpg',
-        preview:
-            "Comming soon...",
-    },
+  {
+    id: 13,
+    title: 'Neroli grapfruit',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/neroli-grapfruit.jpg',
+    preview:
+      'eroli Grapefruit Blossom Essential Oil is steam distilled from gorgeous groves of Grapefruit blossoms in egypt. Similar to our classic Neroli distilled from Orange Blossom, yet with more honeysuckle nuances and more distinct zippy, floral notes. Always uplifting.',
+  },
+
+  {
+    id: 14,
+    title: 'Petitgrain Bigarade',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/petitgrain.jpg',
+    preview:
+      "Our organic Petitgrain Bigarade has a fresh, leafy-green, lightly floral, citrus aroma with light bitter/dry notes and rich, sweet undertones. This essential oil's Neroli-like aroma has made it a classic ingredient of Eau de Cologne formulas.",
+  },
+  {
+    id: 15,
+    title: 'Jasmine Grand oil',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/jasmin-grand.jpeg',
+    preview:
+      'Jasmine Grandiflorum Oil is extracted from Jasminum Gradiflora (synonym officinale) of the Oleaceae family. Extracted from the flower part of the plant through steam distillation process, Jasmine Grandiflorum Oil helps in toning up of body while helping for dry, greasy, irritated and sensitive skin. Jasmine oil helps to tone the skin for a younger look by maintaining the elasticity of skin. This oil, when burnt in oil burner or used with diffuser, lends a divine and spiritual aroma in the interiors that uplifts mind for a relaxed time. Moreover, this oil can also be applied over head for a lustrous shine',
+  },
+  {
+    id: 16,
+    title: 'Thyme oil',
+    href: '#',
+    category: { name: 'Essential oil', href: '#' },
+    imageUrl: './products/thyme.jpg',
+    preview:
+      'Thyme oil comes from the perennial herb known as Thymus vulgaris. This herb is a member of the mint family, and it’s used for cooking, mouthwashes, potpourri and aromatherapy.  It’s native to southern Europe from the western Mediterranean to southern Italy. Due to the herb’s essential oils, it has a number of health benefits; in fact, these benefits have been recognized across the Mediterranean for thousands of years. Thyme oil is antiseptic, antibacterial, antispasmodic, hypertensive and has calming properties.',
+  },
 ]
